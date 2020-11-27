be542045b2eae9e845f43d83a6bff0668e69c6c0
Denis Rajlic <denis.rajlic@gmail.com>
2020-11-27 17:04:51 +0100

    create files for auth & add user registration endpoint

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
